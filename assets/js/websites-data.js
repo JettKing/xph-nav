42144e025ac4f44d758790fc9db9b636711a0bc2
JettKing <149297249+JettKing@users.noreply.github.com>
2026-07-24 07:58:21 +0800

    创建 websites-data.js

---
+++ assets/js/websites-data.js
@@ -0,0 +1,23 @@
+const websitesList = [
+
+{
+icon:"🧰",
+name:"TinyWow",
+desc:"提供大量免费在线文件处理工具",
+category:"online",
+tag:"在线工具",
+website:"https://tinywow.com/",
+project:""
+},
+
+{
+icon:"✂️",
+name:"Remove.bg",
+desc:"快速自动去除图片背景的在线工具",
+category:"online",
+tag:"在线工具",
+website:"https://www.remove.bg/",
+project:""
+}
+
+];
