aaebf0f7506e335fcdb0c56b15e6172c039a0aea
JettKing <149297249+JettKing@users.noreply.github.com>
2026-07-24 08:31:37 +0800

    创建 treasures-data.js

---
+++ assets/js/treasures-data.js
@@ -0,0 +1,23 @@
+const treasuresList = [
+
+{
+icon:"✏️",
+name:"Excalidraw",
+desc:"简洁好用的手绘风在线白板工具",
+category:"creation",
+tag:"创作工具",
+website:"https://excalidraw.com/",
+project:""
+},
+
+{
+icon:"🌍",
+name:"Radio Garden",
+desc:"转动地球即可收听世界各地电台",
+category:"website",
+tag:"宝藏网站",
+website:"https://radio.garden/",
+project:""
+}
+
+];
