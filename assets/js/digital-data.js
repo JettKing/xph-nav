6ba6d36cf98a43e54e2d9850627afa55b14dffbe
JettKing <149297249+JettKing@users.noreply.github.com>
2026-07-24 08:30:47 +0800

    创建 digital-data.js

---
+++ assets/js/digital-data.js
@@ -0,0 +1,23 @@
+const digitalList = [
+
+{
+icon:"💬",
+name:"PromptHero",
+desc:"发现和探索优质 AI 提示词",
+category:"prompt",
+tag:"提示词",
+website:"https://prompthero.com/",
+project:""
+},
+
+{
+icon:"🖼️",
+name:"unDraw",
+desc:"免费可自定义的开源插画素材库",
+category:"design",
+tag:"设计资源",
+website:"https://undraw.co/",
+project:""
+}
+
+];
